857404403133955ffd67deb76a03424ef2ffb0b8
“Jingwen <xiajingwen77@gmail.com>
2022-08-08 19:49:19 -0400

    install styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,11 @@ import History from './History'
 import { Route,Routes } from "react-router-dom";
 import Header from './Header'
 import Footer from './Footer'
+import styled from "styled-components";
 const App = () =>{
 
   return (
-    <div>
+    <Wrapper>
       <Header/>
       <Routes>
         <Route exact path="/" element={<Homepage/>}/>
@@ -18,8 +19,12 @@ const App = () =>{
         <Route path="">404</Route>
       </Routes>
     <Footer/>
-    </div>
+    </Wrapper>
   );
 }
 
+const Wrapper = styled.div`
+  height: 100vh;
+  width:auto;
+`
 export default App;
